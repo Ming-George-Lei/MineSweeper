20bf8a72439c974fa5d150f38b16b5901203ffb6
George <minggeorgelei@gmail.com>
2025-07-18 20:10:17 +0800

    add out dir to the repo

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/minglei/Documents/git/little-game/app/layout.tsx
+// File: /Users/minglei/Documents/git/MineSweeper/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
